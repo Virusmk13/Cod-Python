2c9d4debf836e930fa1532c0a06d12fbd444b56e
Ilie Tudor <147079698+Virusmk13@users.noreply.github.com>
2025-08-12 15:32:10 +0300

    Update cod2.py

--- cod2.py
+++ cod2.py
@@ -1,5 +1,7 @@
 import boto3
 import os
+from datetime import datetime
+import shutil
 
 def upload_to_s3(file_path, bucket_name, s3_key):
     s3 = boto3.client('s3')
@@ -14,8 +16,17 @@ def upload_to_s3(file_path, bucket_name, s3_key):
         print(f"Eroare la urcarea fișierului: {e}")
 
 if __name__ == "__main__":
-    file_name = "output.log"
-    bucket = "numele-tau-bucket"
-    s3_key = f"rapoarte/{file_name}"
+    original_file_name = "output.log"
 
-    upload_to_s3(file_name, bucket, s3_key)
+    if not os.path.exists(original_file_name):
+        raise FileNotFoundError(f"Fișierul {original_file_name} nu există.")
+
+    timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+    renamed_file = f"output_{timestamp}.log"
+
+    shutil.copyfile(original_file_name, renamed_file)
+
+    bucket = "tudoriliebucket"
+    s3_key = f"outputs/{renamed_file}"
+
+    upload_to_s3(renamed_file, bucket, s3_key)
